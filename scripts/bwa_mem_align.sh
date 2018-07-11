81f09660e0749e467ca895d59f7e2e819f69fb33
ChaochihL <liux1299@umn.edu>
2018-07-11 14:39:03 -0400

    correct usage message

--- scripts/bwa_mem_align.sh
+++ scripts/bwa_mem_align.sh
@@ -9,13 +9,10 @@ Usage: ./bwa_mem_align.sh [DEP_DIR] [REFERENCE] [SAMPLE_LIST] [OUT_DIR] \n\
 \n\
 Where: \n\
     1) [DEP_DIR] is the full filepath to the directory containing dependencies (i.e. /path/to/bin)
-    2) [REFERENCE] is a list of full filepaths to sam files
-    3) [SAMPLE_LIST] is the full filepath to the reference.fa file
+    2) [REFERENCE] is the full filepath to the reference.fa file
+    3) [SAMPLE_LIST] is a list of full filepaths to gzipped fastq files
     4) [OUT_DIR] is the full filepath to our output directory
     \n\
-    Example directory tree: if OUT_DIR=/path/to/file, output files will be in
-    OUT_DIR/SAMTools (/path/to/file/SAMTools). The SAMTools directory automatically
-    gets created within this script.
 
 Dependencies: \n\
     1) bwa
